acd8c31de70f849ae8ca2787132860b6f767ae9d
dias <dias.akbar@jalin.co.id>
2025-10-24 14:13:50 +0700

    sign in anon

--- karuba-online/src/firebase.ts
+++ karuba-online/src/firebase.ts
@@ -1,16 +1,7 @@
 import { initializeApp } from "firebase/app";
-// import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth"
-// import type { User } from "firebase/auth"
 import { getDatabase, ref, onValue, set, update, get, child, push, serverTimestamp } from "firebase/database";
-// import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "xxx",
-//   authDomain: "xxx.firebaseapp.com",
-//   databaseURL: "https://decryptor-game-default-rtdb.firebaseio.com",
-//   projectId: "decryptor-game",
-//   appId: "xxx"
-// };
 const firebaseConfig = {
   apiKey: "AIzaSyCbZ5Ca6sMcCdYTE5A1hCq6Vr3NJvPotAQ",
   authDomain: "decryptor-game.firebaseapp.com",
@@ -23,13 +14,37 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// export const auth = getAuth(app);
+
+const auth = getAuth();
+signInAnonymously(auth)
+  .then(() => console.log("Signed in anonymously"))
+  .catch((error) => console.error("Auth error:", error));
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User UID:", user.uid);
+  }
+});
+
 export const db = getDatabase(app);
 
+export { ref, onValue, set, update, get, child, push, serverTimestamp };
+
+
+// import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth"
+// import type { User } from "firebase/auth"
+// import { getAnalytics } from "firebase/analytics";
+
+// const firebaseConfig = {
+//   apiKey: "xxx",
+//   authDomain: "xxx.firebaseapp.com",
+//   databaseURL: "https://decryptor-game-default-rtdb.firebaseio.com",
+//   projectId: "decryptor-game",
+//   appId: "xxx"
+// };
+// const analytics = getAnalytics(app);
+// export const auth = getAuth(app);
 // export async function ensureAuth(): Promise<User> {
 //   if (!auth.currentUser) await signInAnonymously(auth);
 //   return new Promise(resolve => onAuthStateChanged(auth, u => u && resolve(u)));
 // }
-
-export { ref, onValue, set, update, get, child, push, serverTimestamp };
