64d7e1b127eb9496dcaafeb0808c13ab58dd18e5
dias <dias.akbar@jalin.co.id>
2025-10-27 17:30:04 +0700

    fix

--- rancang/src/App.tsx
+++ rancang/src/App.tsx
@@ -1,18 +1,8 @@
 // src/App.tsx
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Lobby from './pages/Lobby';
-import Room from './pages/Room';
+// Temporarily simplified to focus on core game without routing
 import Demo from './pages/Demo';
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Lobby />} />
-        <Route path="/room/:gameId" element={<Room />} />
-        <Route path="/demo" element={<Demo />} />
-      </Routes>
-    </Router>
-  );
+  return <Demo />;
 }
