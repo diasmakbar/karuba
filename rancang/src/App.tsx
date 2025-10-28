4492e4517d9f2aeb36018b100b194f8dd9a11e0a
dias <dias.akbar@jalin.co.id>
2025-10-28 13:48:56 +0700

    delete rancang

--- rancang/src/App.tsx
+++ rancang/src/App.tsx
@@ -1,9 +1,5 @@
 // src/App.tsx
 
-// Simplified to Hello World
-import React from 'react';
-// import Demo from './pages/Demo';
-
 export default function App() {
   return <div>Hello World</div>;
 }
