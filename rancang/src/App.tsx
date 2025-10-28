b4670ca3f501eeed54d4d177f92342b99e9c68fb
dias <dias.akbar@jalin.co.id>
2025-10-28 13:46:52 +0700

    revert rancang to basic

--- rancang/src/App.tsx
+++ rancang/src/App.tsx
@@ -1,8 +1,9 @@
 // src/App.tsx
 
-// Temporarily simplified to focus on core game without routing
-import Demo from './pages/Demo';
+// Simplified to Hello World
+import React from 'react';
+// import Demo from './pages/Demo';
 
 export default function App() {
-  return <Demo />;
+  return <div>Hello World</div>;
 }
